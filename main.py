24b102136e1166aed7c52734b566a11ab1966865
Gabriel Teixeira <139800510+joaogabrielts@users.noreply.github.com>
2025-03-28 15:26:41 -0300

    feat: tarefa x finalizada

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from management.product_handler import get_product_by_id
 from management.product_handler import get_products_by_type
+from management.product_handler import add_product
+from management.product_handler import calculate_tab
+from menu import products
 
 
 if __name__ == "__main__":
@@ -9,3 +12,26 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     ...
     print(get_products_by_type('drink'))
+
+
+if __name__ == "__main__":
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food"
+    }
+    print('add_product', add_product(products, **new_product))
+
+
+if __name__ == "__main__":
+    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    table_2 = [
+        {"_id": 10, "amount": 3},
+        {"_id": 20, "amount": 2},
+        {"_id": 21, "amount": 5},
+    ]
+
+    print('calcula', calculate_tab(table_1))
+    print(calculate_tab(table_2))
--- management/product_handler.py
+++ management/product_handler.py
@@ -16,3 +16,23 @@ def get_products_by_type(product_type: str) -> list:
         if product['type'] == product_type:
             result.append(product)
     return result
+
+
+def add_product(menu: list, **product_data):
+
+    new_id = max((item["_id"] for item in menu), default=0) + 1
+
+    product_data["_id"] = new_id
+
+    menu.append(product_data)
+
+    return product_data
+
+
+def calculate_tab(consumption: list):
+    from menu import products 
+    product_dict = {item["_id"]: item for item in products}
+    subtotal = sum(product_dict[item["_id"]]["price"]
+                   * item["amount"] for item in consumption)
+
+    return {"subtotal": f"${round(subtotal, 2)}"}
